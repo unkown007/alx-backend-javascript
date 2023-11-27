2144c9e3cab95d663aa67c00174949a162944005
unkown007 <victormatusse00@gmail.com>
2023-11-27 21:53:04 +0200

    added file

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,10 @@
+export default function createReportObject(employeesList) {
+  const report = {
+    allEmployees: employeesList,
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
+
+  return report;
+}
