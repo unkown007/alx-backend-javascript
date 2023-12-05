e18ab9b9041fc37ac7e37a88bc5d067cfcaed744
unkown007 <victormatusse00@gmail.com>
2023-12-05 11:44:31 +0200

    added file

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
   if (position >= length) {
-    throw new Error('Position  outside range');
+    throw new Error('Position outside range');
   }
 
   const buff = new DataView(new ArrayBuffer(length), 0, length);
