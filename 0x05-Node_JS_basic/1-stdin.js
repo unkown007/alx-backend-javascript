48725e8fd09cb667316395a996650ccfdecabd8d
unkown007 <victormatusse00@gmail.com>
2024-06-04 20:55:45 +0200

    added file

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('readable', () => {
+  const data = process.stdin.read();
+
+  if (data) {
+    process.stdout.write(`Your name is: ${data}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
